bb09edda81d07b6d7a4e354bdfacfcc8c36adbb2
Stuart Harris <stuart.harris@red-badger.com>
2022-07-17 21:15:52 +0100

    read and write hash

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ use b2sum_rs::Blake2bSum;
 use clap::Parser;
 use globset::{Glob, GlobSetBuilder};
 use ignore::Walk;
-use serde::Deserialize;
-use std::{fs, path::PathBuf};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::{self, File, OpenOptions},
+    io::{BufWriter, Read},
+    path::PathBuf,
+    str::FromStr,
+};
+
+const CACHE_DIR: &str = ".boost";
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -21,6 +28,11 @@ struct Config {
     run: Option<String>,
 }
 
+#[derive(Serialize, Deserialize, PartialEq)]
+struct Hash {
+    value: String,
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
     let file = fs::read(args.file).context("opening file")?;
@@ -33,20 +45,41 @@ fn main() -> Result<()> {
     let set = builder.build()?;
     let context = Blake2bSum::new(16);
     let mut all: Vec<u8> = Vec::new();
-    let files = Walk::new("./")
+    for file in Walk::new("./")
         .flatten()
         .map(|f| f.into_path())
         .filter(|f| f.is_file() && set.is_match(f))
-        .map(|f| {
-            (f.clone(), {
-                let hex = context.read(f);
-                all.extend(Blake2bSum::as_bytes(&hex));
-                hex
-            })
-        })
-        .collect::<Vec<_>>();
-    println!("{:#?}", files);
-    println!("{:#?}", context.read_bytes(&all));
+    {
+        let hex = context.read(file);
+        all.extend(Blake2bSum::as_bytes(&hex));
+    }
+
+    fs::create_dir_all(CACHE_DIR)?;
+    let path = PathBuf::from_str(CACHE_DIR)?.join("hash");
+
+    let previous: Hash = {
+        let mut f = File::open(&path)?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)
+            .with_context(|| format!("reading {}", path.to_string_lossy()))?;
+        serde_json::from_str(&s)?
+    };
+
+    let current = Hash {
+        value: context.read_bytes(&all),
+    };
+
+    if current != previous {
+        println!("inputs have changed");
+        let f = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(&path)
+            .with_context(|| format!("opening {} for writing", path.to_string_lossy()))?;
+        let writer = BufWriter::new(&f);
+        f.set_len(0)?;
+        serde_json::to_writer(writer, &current)?;
+    }
 
     println!("{:?}", config.outputs);
     println!("{:?}", config.run);
