39b8eb4444501709dca53f68d7e72efd4d3c9041
Stuart Harris <stuart.harris@red-badger.com>
2022-07-24 18:28:38 +0100

    add test.toml

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Config {
     pub inputs: Vec<Input>,
-    pub outputs: Vec<Output>,
+    pub outputs: Option<Vec<Output>>,
     pub run: String,
 }
 
